1397633c149d1fcc5fe759f8ed2dcb8c284a2dad
trollboy <trollboy@shoggoth.net>
2015-03-01 01:20:19 -0800

    trying to make angular work

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
+console.log('main.js loaded');
+var myApp = angular.module('peanut',[]);
 
+myApp.controller('HeadingController', ['$scope', function($scope) {
+	console.log('Controller Loaded');
+  $scope.greeting = 'Hola!';
+  $scope.menus = {
+  	'account':false,
+  	'gaming':false,
+  	'literature':false,
+  	'conventions':false,
+  	'editorials':false,
+  	'comedy':false,
+  	'news':false 
+  };
+  console.log($scope.menus);
+}]);
