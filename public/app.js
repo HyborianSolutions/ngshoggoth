62aae1744b4fc3d720b0fa77c5b510f9da98c31c
trollboy <trollboy@shoggoth.net>
2015-03-09 02:30:23 -0700

    added view switcher

--- public/app.js
+++ public/app.js
@@ -28,7 +28,7 @@ app.controller('CarouselDemoCtrl', ['$scope', function ($scope) {
   $scope.myInterval = 5000;
   var slides = $scope.slides = [];
   $scope.addSlide = function() {
-    var newWidth = 800 + slides.length + 1;
+    var newWidth = 900 + slides.length + 1;
     slides.push({
       image: 'http://placekitten.com/' + newWidth + '/300',
       text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
